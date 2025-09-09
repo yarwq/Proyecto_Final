242c3a8f2214f5eb11ab24657a456aff3dc55286
yarwq <yarwq05@gmail.com>
2025-09-09 16:57:56 -0200

    delete comments

--- config.php
+++ config.php
@@ -2,13 +2,13 @@
 $host = "localhost";
 $port = 3306;      
 $user = "root";    
-$pass = "";        // пустой пароль
+$pass = "";        //sin contrasena
 $db   = "draftosaurus";
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
-    die("Ошибка подключения к базе данных: " . $conn->connect_error);
+    die("error para conectar " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4");
