23d7886e0377a6ef7435e7331ac30822bf44fbba
yarwq <yarwq05@gmail.com>
2025-09-15 01:08:48 -0200

    configure api

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -4,18 +4,20 @@ use Controllers\UserController;
 require_once __DIR__ . "/../controllers/UserController.php";
 
 header("Content-Type: application/json; charset=UTF-8");
+
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
-// убираем префикс, если хочешь проще
-$path = str_replace('/Proyecto_Final/api', '', $path);
+
+$segments = explode('/', trim($path, '/'));
+$last = end($segments);
 
 switch (true) {
-    case ($path === '/register' && in_array($method, ['POST','OPTIONS'])):
+    case ($last === 'register' && in_array($method, ['POST','OPTIONS'])):
         (new UserController())->register();
         break;
 
-    case ($path === '/login' && in_array($method, ['POST','OPTIONS'])):
+    case ($last === 'login' && in_array($method, ['POST','OPTIONS'])):
         (new UserController())->login();
         break;
 
